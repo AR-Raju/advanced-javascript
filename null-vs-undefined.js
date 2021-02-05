108fd4b8ee88f33b4b981e7ee2887f9a81fea180
AR-Raju <asikurrahmanraju@gmail.com>
2021-02-05 07:17:31 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+  return;
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+let premik = { name: "smart dude", age: 12 };
+console.log(premik.gf);
+
+let age = [1, 2, 34];
+console.log(age[11]);
+
+we can get null by defining it to a variable
